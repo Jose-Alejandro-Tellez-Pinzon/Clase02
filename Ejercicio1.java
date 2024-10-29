98299d32ab6eec16615acfeb4b29507ebf1a163b
Jose Alejandro Tellez Pinzon <jositotellezpinzon@gmail.com>
2024-10-29 00:04:44 -0500

    Ejercicios

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,3 +1,9 @@
 public class Ejercicio1 {
-    
+    public static void main(String[] args) {
+
+        var operacion = 6 / 2 * (1+2);
+
+        System.out.printf("El resultado de la operacion es: " + operacion);
+
+    }
 }
